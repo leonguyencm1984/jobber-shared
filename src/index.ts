4d0e2a5a7388405b4413f5586b27d92fa1971939
Nguyen Nhan <leonguyencm1984@gmail.com>
2024-04-12 18:44:37 +0700

    add interfaces

---
+++ src/index.ts
@@ -0,0 +1,74 @@
+export {
+    IAuthPayload,
+    IAuth,
+    IAuthDocument,
+    IAuthBuyerMessageDetails,
+    IEmailMessageDetails,
+    ISignUpPayload,
+    ISignInPayload,
+    IForgotPassword,
+    IResetPassword,
+    IReduxAuthPayload,
+    IReduxAddAuthUser,
+    IReduxLogout,
+    IAuthResponse,
+    IAuthUser
+} from './interfaces/auth.interface'
+
+export {
+    IBuyerDocument,
+    IReduxBuyer
+} from './interfaces/buyer.interface';
+
+export {
+    IConversationDocument,
+    IMessageDocument,
+    IMessageDetails,
+    IChatBoxProps,
+    IChatSellerProps,
+    IChatBuyerProps,
+} from './interfaces/chat.interface';
+
+export { IEmailLocals } from './interfaces/email.interface';
+
+export {
+    GigType,
+    ICreateGig,
+    ISellerGig,
+    IGigContext,
+    IGigsProps,
+    IGigCardItems,
+} from './interfaces/gig.interface';
+
+export {
+    IOffer,
+    IExtendedDelivery,
+    IDeliveredWork,
+    IOrderEvents,
+    IOrderReview,
+    IOrderMessage,
+} from './interfaces/order.interface';
+
+export {
+    IReviewMessageDetails,
+    IRatingTypes,
+    IReviewDocument,
+    IRatingCategories,
+    IRatingCategoryItem,
+} from './interfaces/review.interface';
+
+export {
+    ISearchResult,
+    IHitsTotal,
+    IQueryList,
+    IPaginateProps,
+} from './interfaces/search.interface';
+
+export {
+    SellerType,
+    ILanguage,
+    IExperience,
+    IEducation,
+    ICertificate,
+    ISellerDocument,
+} from './interfaces/seller.interface';
